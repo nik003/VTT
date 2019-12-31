5b065e4f0c58b7cb0f95614763b9061aceb2b89b
Ookami <ookami@ookamiprojects.com>
2019-12-31 21:17:27 +0100

    Added starting of refactor

--- server.js
+++ server.js
@@ -3,61 +3,60 @@ var app = express();
 const http = require('http').createServer(app)
 const path = require('path');
 const index = require("./routes/index.js");
+const vtt = require("./routes/vtt.js");
 const cookieParser = require("cookie-parser");
 const bodyparser = require("body-parser");
 const mustacheExpress = require('mustache-express');
-var io = require('socket.io')(http);
-io.on('connection', function(socket){
-  console.log('a user connected');
-	socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-	socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-//const app = express();
-app.use(bodyparser.urlencoded({ extended: false }))
+
+
+app.use(bodyparser.urlencoded({
+  extended: false
+}))
+app.use("/vtt", vtt);
 app.use("/", index);
-app.set('views', [path.join(__dirname, '/VTT/views'),path.join(__dirname,'/views')]);
+app.set('views', [path.join(__dirname, '/views'), path.join(__dirname, '/views')]);
 app.use(express.static(path.join(__dirname, '/views')));
-app.use('/vtt/css',express.static(path.join(__dirname,'/VTT/views/css')));
+app.use('/css', express.static(path.join(__dirname, '/views/css')));
 app.engine('html', mustacheExpress());
 
 
 app.use(cookieParser());
 app.set('view engine', 'html');
 process.env.TZ = 'Europe/Stockholm';
+process.on('uncaughtException', (err) => {
+
+
+});
 
 
 app.all("/*", function(req, res, next) {
 
 
-	next();
+  next();
 });
 
 // Catch 404 and forward to error handler
 app.use(function(err, req, res, next) {
-    err.status = 404;
-    next(err);
+  err.status = 404;
+  next(err);
 });
- // Exception handling
+// Exception handling
 if (app.get("env") === "development") {
-    console.log("In development mode");
-    app.use(function(err, req, res, next) {
-        console.log("Error:" + err.stack);
-        res.status(err.status || 500);
-				res.send("error");
-    });
+  console.log("In development mode");
+  app.use(function(err, req, res, next) {
+    console.log("Error:" + err.stack);
+    res.status(err.status || 500);
+    res.send("error");
+  });
 } else {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.send("error");
-    });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send("error");
+  });
 }
-	console.log("before listening");
-	http.listen(80, ()=>{
-	console.log("Listening at port 8080");
+
+http.listen(8080, () => {
+  console.log("Listening at port 8080");
 
 });
 
